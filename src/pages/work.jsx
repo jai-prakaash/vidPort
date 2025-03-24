cfec7643cff3bf012c939a82eb95dd8b5b18d18f
jai-prakaash <jaisjs123@gmail.com>
2025-03-24 23:01:39 +0530

    sdas

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -6,11 +6,7 @@ function Works() {
       <div>
         <h2>My Video</h2>
         <video width="200" controls autoPlay  className="rounded-2xl">
-<<<<<<< HEAD
         <source src="https://jai-prakaash.github.io/vidPort/videos/b.webm" type="video/webm" />
-=======
-          <source src="https://jai-prakaash.github.io/vidPort/videos/b.webm" type="video/webm" />
->>>>>>> 611825aca8a971641bdfd38f07b274b9a4dd2d89
           Your browser does not support the video tag.
         </video>
       </div>

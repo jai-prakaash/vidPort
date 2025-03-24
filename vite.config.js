b0ab3b049a26b0b38554a089cebec0a39afec0b2
jai-prakaash <jaisjs123@gmail.com>
2025-03-24 22:13:58 +0530

    cghanges

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/',  // Make sure the base is set here
+  base: '/vidPort/',  // Make sure the base is set here
 });

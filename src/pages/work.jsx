6693d2e621186ef3b9b9a544d73ce9c8c79c7367
jai-prakaash <jaisjs123@gmail.com>
2025-03-24 23:31:27 +0530

    cureve video

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Works = () => {
   return (
-    <div>
-      <video width="320" controls>
+    <div> 
+      My video
+      <video width="200" controls autoPlay className=" rounded-2xl" >
         <source src={`${import.meta.env.BASE_URL}/videos/b.webm`} type="video/webm" />
         Your browser does not support the video tag.
       </video>

611825aca8a971641bdfd38f07b274b9a4dd2d89
Jai Prakash <149857548+jai-prakaash@users.noreply.github.com>
2025-03-24 22:57:59 +0530

    Update work.jsx
    d

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -6,7 +6,7 @@ function Works() {
       <div>
         <h2>My Video</h2>
         <video width="200" controls autoPlay  className="rounded-2xl">
-          <source src="/videos/b.webm" type="video/webm" />
+          <source src="https://jai-prakaash.github.io/vidPort/videos/b.webm" type="video/webm" />
           Your browser does not support the video tag.
         </video>
       </div>

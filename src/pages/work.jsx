057eed9144d0b5a3ec4ae93ad7b056ffb0db10a4
jai-prakaash <jaisjs123@gmail.com>
2025-03-24 23:27:24 +0530

    added video lin relative

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-function Works() {
+const Works = () => {
   return (
-    <>
-      <div>
-        <h2>My Video</h2>
-        <video width="200" controls autoPlay  className="rounded-2xl">
-        <source src="https://jai-prakaash.github.io/vidPort/videos/b.webm" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </>
+    <div>
+      <video width="320" controls>
+        <source src={`${import.meta.env.BASE_URL}/videos/b.webm`} type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   );
-}
+};
 
 export default Works;
+

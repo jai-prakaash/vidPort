aeef67722ea6a4b82a723ae37e6c568560337446
jai-prakaash <jaisjs123@gmail.com>
2025-03-26 16:34:40 +0530

    se

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -9,9 +9,9 @@ function Hero(){
   <img
     src={`${import.meta.env.BASE_URL}images/uwp4705937.jpeg`}
     alt="heroimg"
-    className="hero-img object-cover w-full h-auto max-w-screen-lg"
+    className="hero-img object-cover w-full h-screen"
   />
-  <p className="absolute text-3xl text-amber-300 font-bold z-10 text-center px-4 font-custom text-stroke">
+  <p className="absolute text-6xl text-amber-300 font-bold z-10 text-center px-4 font-custom text-stroke">
     Sameer Kumar <br /> Video Editor
   </p>
 </div>

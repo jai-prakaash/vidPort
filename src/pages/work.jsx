bfe5a3cdad2d1584512f20a2d8d0d501135476c7
jai-prakaash <jaisjs123@gmail.com>
2025-03-26 19:00:07 +0530

    10 - 6 video

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -63,49 +63,8 @@ const Works = () => {
 
     </div >
 
-    <div className=" flex justify-around  mt-[2%]" >
-    <div className="flex flex-col items-center "> 
-      My video7
-      <video width="200" controls autoPlay className=" rounded-2xl" >
-        <source src={`${import.meta.env.BASE_URL}/videos/b.webm`} type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-
-    <div className="flex flex-col items-center "> 
-      My video8
-      <video width="200" controls autoPlay className=" rounded-2xl" >
-        <source src={`${import.meta.env.BASE_URL}/videos/b.webm`} type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
 
 
-    <div className="flex flex-col items-center "> 
-      My video9
-      <video width="200" controls autoPlay className=" rounded-2xl" >
-        <source src={`${import.meta.env.BASE_URL}/videos/b.webm`} type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-
-    </div>
-
-
-
-    <div className=" flex justify-around  mt-[2%] " >
-
-    <div className="flex flex-col items-center "> 
-      My video10
-      <video width="200" controls autoPlay className=" rounded-2xl" >
-        <source src={`${import.meta.env.BASE_URL}/videos/b.webm`} type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-
-
-
-</div>
     </div>
   );
 };
